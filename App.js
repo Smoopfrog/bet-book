8389a3fd2901db06f24e9ac707cba94025df7213
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-25 13:12:36 -0800

    create NewBet component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/NewBet.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const NewBet = () => {
+  return (
+    <View>
+      <Text>Bet area</Text>
+    </View>
+  )
+}
+
+export default NewBet;
