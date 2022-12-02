d938d8dcc8e85370217d3b5da4ce505dea896a58
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-12-02 11:47:04 -0800

    change winning percentage formula

--- components/StatBar.js
+++ components/StatBar.js
@@ -3,23 +3,26 @@ import * as Progress from "react-native-progress";
 
 const StatBar = ({ bets }) => {
   const totalBets = bets.length;
-  const betsWon = bets.filter((bet) => bet.result === 'winner').length;
-  const betsLost = bets.filter((bet) => bet.result === 'loser').length;
+  const betsWon = bets.filter((bet) => bet.result === "winner").length;
+  const betsLost = bets.filter((bet) => bet.result === "loser").length;
+  const betsPending = bets.filter((bet) => bet.result === "pending").length;
   const betsSettled = bets.filter((bet) => !bet.active).length;
-
-  const winningPercentage = betsWon / totalBets;
+  const winningPercentage = betsWon / (totalBets - betsPending);
 
   return (
     <View style={styles.container}>
       <Text>Total Bets: {totalBets}</Text>
       <Text>Unsettled bets: {betsSettled}</Text>
       <Text>Winning Percentage</Text>
-      <Progress.Bar
-        color={"green"}
-        unfilledColor={"red"}
-        progress={winningPercentage}
-        width={200}
-      />
+      <View style={styles.porgressCircleContainter}>
+        <Progress.Circle
+          color={"green"}
+          unfilledColor={"red"}
+          progress={winningPercentage}
+          size={100}
+          showsText={true}
+        />
+      </View>
     </View>
   );
 };
@@ -29,6 +32,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     height: "20%",
+  },
+  porgressCircleContainter: {
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 export default StatBar;
