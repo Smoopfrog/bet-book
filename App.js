128eb063f5b1d24327dbb0dd425658f77d019130
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-03-23 19:34:39 -0700

    add react navigation

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StyleSheet, View } from "react-native";
 import Home from "./components/Home/Home";
 import LoginScreen from "./components/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.app}>
-      <LoginScreen />
-      {/* <Home /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ title: 'BETBOOK' }} />
+        <Stack.Screen name="Home" component={Home} options={{ title: 'BETBOOK' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
