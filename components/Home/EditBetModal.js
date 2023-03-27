05e5c53014977b418eed4c5fd487598e4e5ba620
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-03-27 16:10:28 -0700

    delete bet off firebase function created

--- components/Home/EditBetModal.js
+++ components/Home/EditBetModal.js
@@ -1,12 +1,32 @@
 import { View, StyleSheet, Text } from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
 import ModalCard from "./ModalCard";
+import { set, ref } from "firebase/database";
+import { db } from "../../firebase";
 
-const EditBetModal = ({ showModal, closeModal, bet}) => {
+const EditBetModal = ({ showModal, closeModal, bet }) => {
+  const deleteBet = () => {
+    set(ref(db, "bets/" + bet.id), null)
+      .then(() => {
+        alert("Bet deleted");
+        closeModal()
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
 
   return (
     <ModalCard showModal={showModal} closeModal={closeModal}>
       <View style={styles.modalView}>
         <Text>Bet {bet.wager}</Text>
+        <Text>Bet {bet.wager}</Text>
+        <TouchableHighlight
+          onPress={deleteBet}
+          style={styles.deleteButton}
+        >
+          <Text style={styles.deleteButtonText}>Delete</Text>
+        </TouchableHighlight>
       </View>
     </ModalCard>
   );
@@ -29,6 +49,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
+  },
+  deleteButton: {
+    backgroundColor: "red",
+    borderRadius: 6,
+    padding: 6,
+  },
+  deleteButtonText: {
+    color: "white",
   },
 });
 
