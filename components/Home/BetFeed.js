0bea14b744741af10226f60f2bb2c1d89f0bfa43
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-03 13:57:07 -0700

    put scrollview in safeAreaView and fix footer

--- components/Home/BetFeed.js
+++ components/Home/BetFeed.js
@@ -1,22 +1,23 @@
-import {
-  ScrollView,
-  StyleSheet,
-  View,
-  FlatList,
-  SafeAreaView,
-} from "react-native";
+import { ScrollView, StyleSheet, SafeAreaView } from "react-native";
 import Bet from "./Bet";
 
 const BetFeed = ({ bets, setBets }) => {
   const betComponents = bets.map((bet) => {
-    return <Bet key={bet.id} bet={bet} />;
+    return <Bet key={bet.id} bet={bet} setBets={setBets} />;
   });
 
   return (
-    <SafeAreaView>
-      <ScrollView>{betComponents}</ScrollView>
+    <SafeAreaView style={styles.scrollViewContainer}>
+      <ScrollView >
+        {betComponents}
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flex: 1,
+  },
+});
 export default BetFeed;
