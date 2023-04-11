91f1734a6e1281985b9c1cf8270d1b5012628355
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-10 18:08:01 -0700

    change react nav header color

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import LoginScreen from "./components/LoginScreen";
 import ProfileScreen from "./components/ProfileScreen";
 import { Provider } from "react-redux";
 import store from "./store";
+
 const App = () => {
   const [bets, setBets] = useState([]);
 
@@ -18,10 +19,34 @@ const App = () => {
           <Stack.Screen name="LoginScreen" options={{ headerShown: false }}>
             {() => <LoginScreen setBets={setBets} />}
           </Stack.Screen>
-          <Stack.Screen name="HomeScreen" options={{ title: "BETBOOK" }}>
+          <Stack.Screen
+            name="HomeScreen"
+            options={{
+              title: "BETBOOK",
+              headerStyle: {
+                backgroundColor: "#f4511e",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+          >
             {() => <HomeScreen bets={bets} setBets={setBets} />}
           </Stack.Screen>
-          <Stack.Screen name="ProfileScreen" options={{ title: "Profile" }}>
+          <Stack.Screen
+            name="ProfileScreen"
+            options={{
+              title: "Profile",
+              headerStyle: {
+                backgroundColor: "#f4511e",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+          >
             {() => <ProfileScreen bets={bets} />}
           </Stack.Screen>
         </Stack.Navigator>
