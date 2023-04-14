ca9afa23b47c0a75cb6f4c9551c4516871b879e5
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-14 15:18:31 -0700

    add font to profile and recolor

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -2,7 +2,24 @@ import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { auth } from "../firebase";
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
 const ProfileScreen = ({ bets }) => {
+  const [fontsLoaded] = useFonts({
+    "Orbitron-Regular": require("../assets/fonts/Orbitron-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   const calculateDollarWinnings = (bets, result) => {
     let winnings = 0;
     for (const bet of bets) {
@@ -29,6 +46,7 @@ const ProfileScreen = ({ bets }) => {
   const winningPercentage = Math.round(
     (betsWon / (totalBets - betsPending)) * 100
   );
+  console.log("winningPercentage", winningPercentage /*  */);
   const betsPending = bets.filter((bet) => bet.result === "pending").length;
   const betsSettled = bets.filter((bet) => !bet.active).length;
   const moneyWon = calculateDollarWinnings(bets, "winner");
@@ -49,33 +67,85 @@ const ProfileScreen = ({ bets }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.sectionTitle}>ACCOUNT</Text>
-
-      <View style={[styles.sectionContainer, styles.accountSection]}>
-        <Text style={styles.textTitle}>Email</Text>
-        <Text>{auth.currentUser.email}</Text>
+      <View style={styles.sectionTitle}>
+        <Ionicons name="person" size={24} color="white" />
+        <Text
+          style={[
+            styles.sectionTitle,
+            { fontFamily: "Orbitron-Regular", color: "white" },
+          ]}
+        >
+          ACCOUNT
+        </Text>
       </View>
       <View style={[styles.sectionContainer, styles.accountSection]}>
-        <Text style={styles.textTitle}>Logout</Text>
+        <Text
+          style={[
+            styles.textTitle,
+            { fontFamily: "Orbitron-Regular", color: "white" },
+          ]}
+        >
+          Email
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          {auth.currentUser.email}
+        </Text>
+      </View>
+      <View style={[styles.sectionContainer, styles.accountSection]}>
+        <Text
+          style={[
+            styles.textTitle,
+            { fontFamily: "Orbitron-Regular", color: "white" },
+          ]}
+        >
+          Logout
+        </Text>
         <TouchableHighlight style={styles.logoutButton} onPress={logoutHandler}>
           <MaterialIcons name="logout" size={24} color="white" />
         </TouchableHighlight>
       </View>
       <View style={styles.sectionTitle}>
-        <Ionicons name="stats-chart" size={24} color="black" />
-        <Text style={styles.sectionTitleText}>STATS</Text>
+        <Ionicons name="stats-chart" size={24} color="white" />
+        <Text
+          style={[
+            styles.sectionTitleText,
+            { fontFamily: "Orbitron-Regular", color: "white" },
+          ]}
+        >
+          STATS
+        </Text>
       </View>
       <View style={[styles.sectionContainer, styles.statsSection]}>
-        <Text>Total Bets: {totalBets}</Text>
-        <Text>Total Wins: {betsWon}</Text>
-        <Text>Total Loses: {betsLost}</Text>
-        <Text>Total Pending: {betsPending}</Text>
-        <Text>Total Settled: {betsSettled}</Text>
-        <Text>Win Percentage: {winningPercentage}%</Text>
-        <Text>Total Money Won: ${moneyWon}</Text>
-        <Text>Total Money Lost: ${moneyLost}</Text>
-        <Text>Total Winnings: {othersWon ? othersWon : "Nothing"}</Text>
-        <Text>Total Lost: {othersLost ? othersLost : "Nothing"}</Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Bets: {totalBets}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Wins: {betsWon}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Loses: {betsLost}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Pending: {betsPending}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Settled: {betsSettled}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Win Percentage: {winningPercentage}%
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Money Won: ${moneyWon}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Money Lost: ${moneyLost}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Winnings: {othersWon ? othersWon : "Nothing"}
+        </Text>
+        <Text style={{ fontFamily: "Orbitron-Regular", color: "white" }}>
+          Total Lost: {othersLost ? othersLost : "Nothing"}
+        </Text>
       </View>
     </View>
   );
@@ -85,7 +155,7 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     flex: 1,
-    backgroundColor: "#f8f0fc",
+    backgroundColor: "#5f3dc4",
   },
   sectionTitle: {
     width: "90%",
@@ -101,7 +171,7 @@ const styles = StyleSheet.create({
   },
   sectionContainer: {
     width: "90%",
-    backgroundColor: "white",
+    backgroundColor: "black",
     padding: 10,
     borderRadius: 3,
     marginBottom: 5,
