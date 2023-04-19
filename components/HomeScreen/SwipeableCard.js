f7fbf47a62b5ba1b3dacca9f4634755f7d55264a
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-18 22:41:53 -0700

    add font and changge color of swipeable card

--- components/HomeScreen/SwipeableCard.js
+++ components/HomeScreen/SwipeableCard.js
@@ -1,7 +1,23 @@
 import { StyleSheet, Text, View, Animated } from "react-native";
 import { RectButton, Swipeable } from "react-native-gesture-handler";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 const SwipeableCard = (props) => {
+  const [fontsLoaded] = useFonts({
+    "Orbitron-Regular": require("../../assets/fonts/Orbitron-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   const RenderRight = (progress, dragX) => {
     // const scale = dragX.interpolate({
     //   inputRange: [-50, 0.5],
@@ -20,12 +36,20 @@ const SwipeableCard = (props) => {
         <View
           style={{
             width: "100%",
-            backgroundColor: "#FFAC41",
+            backgroundColor: "black",
+            borderColor: "white",
+            borderWidth: 2,
             alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <Animated.Text style={{ color: "#fff", fontWeight: "600" }}>
+          <Animated.Text
+            style={{
+              color: "white",
+              fontWeight: "600",
+              fontFamily: "Orbitron-Regular",
+            }}
+          >
             {result}
           </Animated.Text>
         </View>
@@ -35,12 +59,20 @@ const SwipeableCard = (props) => {
       <View
         style={{
           width: "100%",
-          backgroundColor: "green",
+          backgroundColor: "black",
+          borderColor: "green",
+          borderWidth: 2,
           alignItems: "center",
           justifyContent: "center",
         }}
       >
-        <Animated.Text style={{ color: "#fff", fontWeight: "600" }}>
+        <Animated.Text
+          style={{
+            color: "green",
+            fontWeight: "600",
+            fontFamily: "Orbitron-Regular",
+          }}
+        >
           {result}
         </Animated.Text>
       </View>
@@ -64,12 +96,20 @@ const SwipeableCard = (props) => {
         <View
           style={{
             width: "100%",
-            backgroundColor: "#FFAC41",
+            backgroundColor: "black",
+            borderColor: "white",
+            borderWidth: 2,
             alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <Animated.Text style={{ color: "#fff", fontWeight: "600" }}>
+          <Animated.Text
+            style={{
+              color: "white",
+              fontWeight: "600",
+              fontFamily: "Orbitron-Regular",
+            }}
+          >
             {result}
           </Animated.Text>
         </View>
@@ -80,12 +120,20 @@ const SwipeableCard = (props) => {
       <View
         style={{
           width: "100%",
-          backgroundColor: "red",
+          backgroundColor: "black",
+          borderColor: "red",
+          borderWidth: 2,
           alignItems: "center",
           justifyContent: "center",
         }}
       >
-        <Animated.Text style={{ color: "#fff", fontWeight: "600" }}>
+        <Animated.Text
+          style={{
+            color: "red",
+            fontWeight: "600",
+            fontFamily: "Orbitron-Regular",
+          }}
+        >
           {result}
         </Animated.Text>
       </View>
